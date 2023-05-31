cb04e4dc863ec56ac2adf688946d6454c9027b44
varge567 <135157046+varge567@users.noreply.github.com>
2023-05-31 11:03:04 -0700

    Update ShipTester.java

--- ShipTester.java
+++ ShipTester.java
@@ -1,1 +1,12 @@
-
+public class ShipTester extends ConsoleProgram
+{
+    public void run()
+    {
+        Ship s1 = new Ship(5);
+        System.out.println(s1);
+        s1.setLocation(5, 2);
+        System.out.println(s1);
+        s1.setDirection(0);
+        System.out.println(s1);
+    }
+}
